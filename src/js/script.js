e20e8709080ca3995aa3f18cd828a35ceebe36a8
Constantin <dorianspam@gmail.com>
2021-04-22 13:36:10 +0300

    add caruserl and media 320

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,5 @@
-const slider = tns({
-	container: '.carusel__inner',
-	items: 1,
-	slideBy: 'page',
-	autoplay: false,
-	controls: false,
-	nav: false,
-});
-
-document.querySelector('.prev').addEventListener('click', function () {
-	slider.goTo('prev');
-});
-document.querySelector('.next').addEventListener('click', function () {
-	slider.goTo('next');
+let myCarousel = document.querySelector('#carouselExampleControls');
+let carousel = new bootstrap.Carousel(myCarousel, {
+	interval: false,
+	ride: false,
 });
